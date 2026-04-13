52811a3c0b94f5c539628a19d1d102fea8386e8f
xieyeying <yeying@deepverse.tech>
2026-04-13 11:29:39 +0800

    基础样式

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import ChatInput from "../components/chat/ChatInput";
 import ChatHeader from "../components/chat/ChatHeader";
 import { chatCompletionsRequest } from "../api/chat.ts"; 
@@ -6,6 +6,13 @@ import { chatCompletionsRequest } from "../api/chat.ts";
 type Message = {
   role: "user" | "assistant";
   content: string;
+  model: string;
+  created: number;
+  usage?: {
+    prompt_tokens: number; // 输入给ai的内容的花费 包含system prompt, user message, 历史上下文
+    completion_tokens: number; // ai回复的花费
+    total_tokens: number; // prompt_tokens + completion_tokens
+  }
 }
 
 export default function ChatPage() {
@@ -23,27 +30,43 @@ export default function ChatPage() {
     setMessages((prev) => [...prev, userMsg]);
 
     // 模拟 AI 回复
-    setTimeout(() => {
-      setMessages((prev) => [
+    // setTimeout(() => {
+    //   setMessages((prev) => [
+    //     ...prev,
+    //     {
+    //       role: "assistant",
+    //       content: "我收到了：" + data.content,
+    //     },
+    //   ]);
+    // }, 500);
+    
+    const res = await chatCompletionsRequest(model, [userMsg]);
+    setMessages((prev) =>{
+      const next = [
         ...prev,
         {
-          role: "assistant",
-          content: "我收到了：" + data.content,
+          role: res.choices[0].message.role,
+          content: res.choices[0].message.content,
+          created: res.created,
+          model: res.model,
+          usage: {
+            prompt_tokens: res.usage.prompt_tokens,
+            completion_tokens: res.usage.completion_tokens,
+            total_tokens: res.usage.total_tokens,
+          },
         },
-      ]);
-    }, 500);
-    
-    // const res = await chatCompletionsRequest(model, [userMsg]);
-    // setMessages((prev) => [
-    //   ...prev,
-    //   {
-    //     role: "assistant",
-    //     content: res.choices[0].message.content,
-    //   },
-    // ])
+      ]
+      console.log(next);
+      return next;
+    })
 
     inputRef.current?.clearInput();
   }
+
+  const formatTime = (created: number) => {
+    return new Date(created * 1000).toLocaleString();
+  }
+
   return (
     <div style={styles.chatPageContainer}>
       <ChatHeader value={model} onModelChange={setModel} />
@@ -61,16 +84,27 @@ export default function ChatPage() {
               key={index}
               style={{
                 ...styles.msgContainer,
-                alignItems: isUser ? "flex-end" : "flex-start",
+                justifyContent: isUser ? "flex-end" : "flex-start",
               }}
             >
-              <div
-                style={{
-                  ...styles.msgContent,
-                  background: isUser ? "#f3f3f3" : "#fff",
-                }}
-              >
-                {msg.content}
+              <div style={styles.msgWrapper}>
+                <div
+                  style={{
+                    ...styles.msgContent,
+                    background: isUser ? "#f3f3f3" : "#fff",
+                  }}
+                >
+                  {msg.content}
+                </div>
+                {!isUser && msg.usage && ( // A && B && C  A和B都成立才显示C
+                  <div style={styles.tokenInfo}>
+                    <div>
+                      {formatTime(msg.created)} &nbsp;&nbsp; {msg.model}
+                    </div>
+                    tokens: {msg.usage.total_tokens}  &nbsp;&nbsp;
+                    (in: {msg.usage.prompt_tokens}, out: {msg.usage.completion_tokens})
+                  </div>
+                )}
               </div>
             </div>
           )
@@ -98,9 +132,24 @@ const styles: Record<string, any> = {
     overflowY: "auto",
     marginBottom: 12,
   },
+  msgContainer: {
+    display: "flex",
+    width: "100%",
+  },
   msgContent: {
-    maxWidth: "60%",
-    padding: "10px 14px",
+    padding: "10px 12px",
     borderRadius: 10,
+  },
+  msgWrapper: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  tokenInfo: {
+    fontSize: "11px",
+    color: "#999",
+    textAlign: "left",
+    marginTop: "8px",
+    lineHeight: "14px",
+    paddingLeft: "12px",
   }
 }
